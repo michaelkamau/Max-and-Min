f6d0dfa19e8167d4c9a17a83477abcf8dddcfcb1
Michael Kamau <tokamau@gmail.com>
2016-10-11 00:22:14 +0300

    Add test - should handle single min and max values

--- test/max_mintests.py
+++ test/max_mintests.py
@@ -9,3 +9,8 @@ class MaxMinTest(unittest.TestCase):
         self.assertListEqual([1, 4],
                              find_max_min([1, 2, 3, 4]),
                              msg='should return [1,4] for [1, 2, 3, 4]')
+
+    def test_find_max_min_one(self):
+        self.assertListEqual([4, 6],
+                             find_max_min([6, 4]),
+                             msg='should return [4, 6] for [6, 4]')
